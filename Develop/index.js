0a3403bf3576cf6b59550cfe68b47f527437e2bf
Mrs. Kikta <melissakikta@gmail.com>
2024-11-13 19:59:22 -0500

    initial commit of project with gitignore, index.js, package.json

---
+++ Develop/index.js
@@ -0,0 +1,17 @@
+// Packages for application
+import inquirer from "inquirer";
+
+// TODO: Create an array of questions for user input
+const questions = [
+  inquirer
+    .prompt
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
